fe6017e0bbb85697a849ccabde6dd368e61a6c83
KseniiaS <myNew@email.com>
2023-07-25 23:00:46 +0300

    Corrected_Code_Task_2_2

--- src/main/java/HomeTask_2/Task_2_2.java
+++ src/main/java/HomeTask_2/Task_2_2.java
@@ -1,15 +1,17 @@
 package HomeTask_2;
 
 public class Task_2_2 {
+//    corrected code
     public static void main(String[] args) {
         int [] intArray = {2, 3, 4};
         try {
-            int d = 0;
-            double catchedRes1 = intArray[8] / d;
+            int d = 5;
+            double catchedRes1 =(double) intArray[8] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("You are exceeded Array size");
         }
-
     }
 }
