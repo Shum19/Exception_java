46eb9860650830eb5df158a67cd32a2a276c26d5
KseniiaS <myNew@email.com>
2023-07-25 23:13:21 +0300

    Corrected_Code_Task_2_3

--- src/main/java/HomeTask_2/Task_2_3.java
+++ src/main/java/HomeTask_2/Task_2_3.java
@@ -1,23 +1,23 @@
 package HomeTask_2;
-
+// Corrected code Task_3
 public class Task_2_3 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try {
             int a = 90;
             int b = 3;
             System.out.println(a / b);
             printSum(23, 234);
             int[] abc = { 1, 2 };
-            abc[3] = 9;
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
-        } catch (NullPointerException ex) {
-            System.out.println("Указатель не может указывать на null!");
-        } catch (IndexOutOfBoundsException ex) {
+            abc[0] = 9;
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Массив выходит за пределы своего размера!");
+        }  catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
         }
     }
-    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+    public static void printSum(Integer a, Integer b) {
         System.out.println(a + b);
     }
 
